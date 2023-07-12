562c620f0631ca95067c4ae8d2b33536121721d7
vyogi10 <vyogi@hotmail.com>
2023-07-12 14:17:30 +0100

    Create Book.js

---
+++ src/Book.js
@@ -0,0 +1,9 @@
+class Book {
+  constructor(title, author, isbn, quantity, price) {
+    this.title = name;
+    this.author = year;
+    this.isbn = isbn;
+    this.quality = quality;
+    this.price = price;
+  }
+}
